f1b9877197a1e62d4eb9489a44b65d6c7beafb55
Jan Drapela <153516920+S3PTUN@users.noreply.github.com>
2025-03-28 08:21:05 +0100

    Add files via upload

---
+++ hraTesty/HerniPostava.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hraTesty
+{
+    public class HerniPostava
+    {
+        public string Jmeno, Specializace;
+        public int Level, X, Y, XP;
+
+        public void PridejXP(int xp)
+        { }
+    }
+}
